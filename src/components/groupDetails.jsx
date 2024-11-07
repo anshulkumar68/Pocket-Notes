2000d955d79ae550cc8881f64b6a3e519e635da8
Anshul <anshul3071@gmail.com>
2024-11-07 23:02:20 +0530

    feat : working on group details component

--- src/components/groupDetails.jsx
+++ src/components/groupDetails.jsx
@@ -1,23 +1,60 @@
-import React from 'react'
-import styles from './groupDetails.module.css'
-import { sendArrowPic } from '../data/data'
+import React, { useState } from "react";
+import styles from "./groupDetails.module.css";
+import { sendArrowPic } from "../data/data";
 
-const GroupDetails = () => {
-    return (
-        <>
-            <div className={styles.detailsContainer}>
-                <div className={styles.detailsTop}>TOP</div>
-                <div>Notes Area</div>
-                <div className={styles.detailsBottom}>
-                    <textarea
-                        placeholder="Enter your text here........."
-                       
-                    />
-                    <img className={styles.sendMessage} src={sendArrowPic} alt="send message arrow" />
-                </div>
+const GroupDetails = ({ groupName, groupColor, groupInitial }) => {
+  const [writtenNote, setWrittenNote] = useState("");
+  const [showNote, setShowNote] = useState(false);
+
+  const hanldeUpdateNote = () => {
+    console.log("sent button called");
+    setShowNote(true);
+    console.log(writtenNote);
+  };
+
+  return (
+    <>
+      <div className={styles.detailsContainer}>
+        <div className={styles.detailsTop}>
+          {
+            <div className={styles.groupDetailsContainer}>
+              <div
+                className={styles.listItemLogo}
+                style={{
+                  backgroundColor: groupColor, // Use the selected color
+                }}
+              >
+                {groupInitial}
+              </div>
+              <span className={styles.listItemName}>{groupName}</span>
             </div>
-        </>
-    )
-}
+            
+          }
+        </div>
+        {/* ==NOTE AREA== */}
+        {showNote && (
+          <div className={styles.noteContainer}>
+            <div className={styles.note}>{writtenNote}</div>
+          </div>
+        )}
 
-export default GroupDetails
+        {/* ==TEXTAREA== */}
+        <div className={styles.detailsBottom}>
+          <textarea
+            placeholder="Enter your text here........."
+            value={writtenNote}
+            onChange={(e) => setWrittenNote(e.target.value)}
+          />
+          <img
+            className={styles.sendMessage}
+            src={sendArrowPic}
+            alt="send message arrow"
+            onClick={hanldeUpdateNote}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GroupDetails;
--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -9,23 +9,32 @@ const MainPage = () => {
   const [notes, setNotes] = useState([]);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [showDetails, setShowDetails] = useState(false);
+  const [groupName, setGroupName] = useState(null);
+  const [groupInitial, setGroupInitial] = useState(null);
+  const [groupColor, setGroupColor] = useState(null);
 
   const addNote = (note, color) => {
     setNotes([...notes, { note, color }]); // Add the entered text to notes
   };
 
+  // to handle open pop-up
   const handleOpenPopup = () => {
     setIsPopupOpen(true);
   };
 
+  // to handle close pop-up 
   const handleClosePopup = () => {
     setIsPopupOpen(false);
   };
 
-  const handleShowDetails = () => {
-    console.log("details called");
-    setShowDetails(!showDetails);  // toggle state
-  }
+  // to handle display group details
+  const handleShowDetails = (initials, note, color) => {
+    setShowDetails(!showDetails); // toggle state
+    setGroupName(note);
+    setGroupColor(color);
+    setGroupInitial(initials);
+  };
+
 
   return (
     <>
@@ -36,15 +45,20 @@ const MainPage = () => {
             {notes.map((item, index) => {
               const { note, color } = item;
               const nameParts = note.split(" ");
-              const initials = nameParts.length > 1
-                ? `${nameParts[0][0]}${nameParts[1][0]}`.toUpperCase()
-                : nameParts[0][0].toUpperCase();
+              const initials =
+                nameParts.length > 1
+                  ? `${nameParts[0][0]}${nameParts[1][0]}`.toUpperCase()
+                  : nameParts[0][0].toUpperCase();
 
               return (
                 <>
-                  {/* <li>ANSHUL KUMAR</li> */}
-                  <li key={index} className={styles.listItems} onClick={() => handleShowDetails()}>
-                    <div className={styles.listItemLogo}
+                  <li
+                    key={index}
+                    className={styles.listItems}
+                    onClick={() => handleShowDetails(initials, note, color)}
+                  >
+                    <div
+                      className={styles.listItemLogo}
                       style={{
                         backgroundColor: color, // Use the selected color
                       }}
@@ -53,7 +67,6 @@ const MainPage = () => {
                     </div>
                     <span className={styles.listItemName}>{note}</span>
                   </li>
-
                 </>
               );
             })}
@@ -64,11 +77,18 @@ const MainPage = () => {
             </span>
           </div>
         </div>
+
         {/* Details Component */}
-        {showDetails && <GroupDetails />}
+        {showDetails && (
+          <GroupDetails
+            groupName={groupName}
+            groupColor={groupColor}
+            groupInitial={groupInitial}
+          />
+        )}
 
         {/* RIGHT Side */}
-        {!showDetails &&
+        {!showDetails && (
           <div className={styles.rightSide}>
             <img
               className={styles.bannerImage}
@@ -77,7 +97,6 @@ const MainPage = () => {
             />
             <h1>Pocket Notes</h1>
             <div className={styles.rightSideText}>
-
               <span className={styles.bannerText}>
                 Send and receive messages without keeping your phone online.
               </span>
@@ -90,12 +109,13 @@ const MainPage = () => {
               </div>
             </div>
           </div>
-        }
-
+        )}
       </div>
 
       {/* pop up for add group */}
-      {isPopupOpen && <AddGroup handleClosePopup={handleClosePopup} addNote={addNote} />}
+      {isPopupOpen && (
+        <AddGroup handleClosePopup={handleClosePopup} addNote={addNote} />
+      )}
     </>
   );
 };
