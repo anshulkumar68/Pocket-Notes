82793c2a5c5bb0cf1ca850424bba6bcff5996a95
Groot code <anshul3071@gmail.com>
2024-11-11 17:56:01 +0530

    feat : responsive

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -76,29 +76,12 @@ const MainPage = () => {
       setSelectedGroup(name);
       setShowDetails(true);
     }
-  
+
     // Hide left side on mobile view
     if (isMobileView) {
       document.querySelector(`.${styles.leftSide}`).style.display = "none";
     }
   };
-
-  // const handleShowDetails = (initials, name, color) => {
-  //   if (selectedGroup === name) {
-  //     setShowDetails(!showDetails);
-  //   } else {
-  //     setGroupName(name);
-  //     setGroupColor(color);
-  //     setGroupInitial(initials);
-  //     setSelectedGroup(name);
-  //     setShowDetails(true);
-
-  //     // Hide left side on mobile view
-  //     if (isMobileView) {
-  //       document.querySelector(`.${styles.leftSide}`).style.display = "none";
-  //     }
-  //   }
-  // };
 
   // to handle back button
   const handleBackButtonClick = () => {
@@ -106,7 +89,7 @@ const MainPage = () => {
     if (isMobileView) {
       document.querySelector(`.${styles.leftSide}`).style.display = "block";
     }
-  }; 
+  };
 
   return (
     <>
@@ -143,13 +126,13 @@ const MainPage = () => {
               );
             })}
           </ul>
-          <div className={styles.addButtonContainer1}>
-            <div className={styles.addButtonContainer}>
-              <span className={styles.addButton} onClick={handleOpenPopup}>
-                +
-              </span>
-            </div>
+            
+          <div className={styles.addButtonContainer}>
+            <span className={styles.addButton} onClick={handleOpenPopup}>
+              +
+            </span>
           </div>
+
         </div>
 
         {showDetails ? (
@@ -194,172 +177,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-// import React, { useEffect } from "react";
-// import styles from "./mainPage.module.css";
-// import { bannerPic, privacyPic } from "../data/data";
-// import { useState } from "react";
-// import AddGroup from "./addGroup";
-// import GroupDetails from "./groupDetails";
-
-// const MainPage = () => {
-//   const [notes, setNotes] = useState([]);
-//   const [isPopupOpen, setIsPopupOpen] = useState(false);
-//   const [showDetails, setShowDetails] = useState(false);
-//   const [groupName, setGroupName] = useState(null);
-//   const [groupInitial, setGroupInitial] = useState(null);
-//   const [groupColor, setGroupColor] = useState(null);
-//   const [selectedGroup, setSelectedGroup] = useState("");
-
-//   //  Load notes from localStorage on mount
-//   useEffect(() => {
-//     try {
-//       const storedNotes = JSON.parse(localStorage.getItem("notes")) || [];
-//       setNotes(storedNotes); // to initialize the state with any previously saved notes
-//     } catch (error) {
-//       console.error("Error loading notes from localstorage", error);
-//       setNotes([]); // Fallback to an empty array if there is an error
-//     }
-//   }, []);
-
-//   const addNote = (name, color) => {
-//     // Check if a note with the same name already exists
-//     const existingNote = notes.find(
-//       (note) => note.name.toLowerCase() === name.toLowerCase()
-//     );
-
-//     if (existingNote) {
-//       return; // Prevent adding the note
-//     }
-
-//     try {
-//       const newNotes = [...notes, { name, color }]; // Add the entered text to notes
-//       setNotes(newNotes);
-
-//       // Save updated notes array to localStorage
-//       localStorage.setItem("notes", JSON.stringify(newNotes));
-//     } catch (error) {
-//       console.error("Error saving notes to localStorage:", error);
-//     }
-//   };
-
-//   // to handle open pop-up
-//   const handleOpenPopup = () => {
-//     setIsPopupOpen(true);
-//   };
-
-//   // to handle close pop-up
-//   const handleClosePopup = () => {
-//     setIsPopupOpen(false);
-//   };
-
-//   // to handle display group details
-//   const handleShowDetails = (initials, name, color) => {
-//     if (selectedGroup === name) {
-//       setShowDetails(!showDetails); // toggle state
-//     } else {
-//       setGroupName(name);
-//       setGroupColor(color);
-//       setGroupInitial(initials);
-//       setSelectedGroup(name);
-//     }
-//   };
-
-//   // save user
-//   const saveUser = () => {
-//     localStorage.setItem("user");
-//   };
-
-//   return (
-//     <>
-//       <div className={styles.mainContainer}>
-//         <div className={styles.leftSide}>
-//           <h2>Pocket Notes</h2>
-//           <ul>
-//             {notes.map((item, index) => {
-//               const { name, color } = item;
-//               const nameParts = name.split(" ");
-//               const initials =
-//                 nameParts.length > 1
-//                   ? `${nameParts[0][0]}${nameParts[1][0]}`.toUpperCase()
-//                   : nameParts[0][0].toUpperCase();
-
-//               return (
-//                 <>
-//                   <li
-//                     key={`${name}-${color}`}
-//                     className={styles.listItems}
-//                     onClick={() => handleShowDetails(initials, name, color)}
-//                     style={{
-//                       backgroundColor:
-//                         selectedGroup === name ? "#e5e5e5" : "white",
-//                     }}
-//                   >
-//                     <div
-//                       className={styles.listItemLogo}
-//                       style={{
-//                         backgroundColor: color, // Use the selected color
-//                       }}
-//                     >
-//                       {initials}
-//                     </div>
-//                     <span className={styles.listItemName}>{name}</span>
-//                   </li>
-//                 </>
-//               );
-//             })}
-//           </ul>
-//           <div className={styles.addButtonContainer1}>
-//             <div className={styles.addButtonContainer}>
-//               <span className={styles.addButton} onClick={handleOpenPopup}>
-//                 +
-//               </span>
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* Details Component */}
-//         {showDetails && (
-//           <GroupDetails
-//             groupName={groupName}
-//             groupColor={groupColor}
-//             groupInitial={groupInitial}
-//           />
-//         )}
-
-//         {/* RIGHT Side */}
-//         {!showDetails && (
-//           <div className={styles.rightSide}>
-//             <img
-//               className={styles.bannerImage}
-//               src={bannerPic}
-//               alt="banner image"
-//             />
-//             <h1>Pocket Notes</h1>
-//             <div className={styles.rightSideTextContainer}>
-//               <div className={styles.noteLineContainer}>
-//               <span className={styles.bannerText}>
-//                 Send and receive messages without keeping your phone online.
-//               </span>
-//               <span className={`${styles.bannerText} ${styles.bannerText2}`}>
-//                 Use Pocket Notes on up to 4 linked devices and 1 mobile phone
-//               </span>
-//               </div>
-//               <div className={styles.privacyContainer}>
-//                 <img src={privacyPic} alt="privacy image" width={"12rem"} />
-//                 <span>end-to-end encrypted</span>
-//               </div>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-
-//       {/* pop up for add group */}
-//       {isPopupOpen && (
-//         <AddGroup handleClosePopup={handleClosePopup} addNote={addNote} />
-//       )}
-//     </>
-//   );
-// };
-
-// export default MainPage;
