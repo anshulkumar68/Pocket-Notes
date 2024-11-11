0593ba9f7c442a0c5a5d05618b98315f881059e2
Anshul <anshul3071@gmail.com>
2024-11-11 21:00:26 +0530

    feat : responsive

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -126,13 +126,14 @@ const MainPage = () => {
               );
             })}
           </ul>
-            
-          <div className={styles.addButtonContainer}>
+
+          {/* <div className={styles.addButtonContainer}>
             <p className={styles.addButton} onClick={handleOpenPopup}>
               +
             </p>
-          </div>
+          </div> */}
 
+          <div className={styles.addButtonContainer}>+</div>
         </div>
 
         {showDetails ? (
